44b19a2380a6e21419cfb125949a25d325a84449
Mayara <maay.bs@hotmail.com>
2016-03-19 14:32:24 -0300

    Classe controller

--- ListaDetalhes/src/main/java/pucpr/bsi/prog4/controllers/ProdutoController.java
+++ ListaDetalhes/src/main/java/pucpr/bsi/prog4/controllers/ProdutoController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import pucpr.bsi.prog4.models.Produto;
 import pucpr.bsi.prog4.models.ProdutoService;
 
 @Controller
